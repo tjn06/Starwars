7f22f6bded3e9e05c5f13b527df110724c9474f4
Tobias <tobb-on@hotmail.com>
2020-05-13 06:01:54 +0200

    Updates

---
+++ precache-manifest.a61d9c2352a3e8894eb0fafedf4908b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf865d01bbb6b588577175d6c320fa",
+    "url": "/Starwars/index.html"
+  },
+  {
+    "revision": "a5c3e4fbc9ce3ad8c604",
+    "url": "/Starwars/static/css/main.734a486e.chunk.css"
+  },
+  {
+    "revision": "e06384b7f86745a08b77",
+    "url": "/Starwars/static/js/2.28d53c4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Starwars/static/js/2.28d53c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c3e4fbc9ce3ad8c604",
+    "url": "/Starwars/static/js/main.42443c14.chunk.js"
+  },
+  {
+    "revision": "e4f43d7b6c59b844aedb",
+    "url": "/Starwars/static/js/runtime-main.980ed8d2.js"
+  },
+  {
+    "revision": "6575da5aa450ca4aad46f637e962b35f",
+    "url": "/Starwars/static/media/dart.6575da5a.jpg"
+  },
+  {
+    "revision": "c91748d9d3169f711d5f2d38523a8d9b",
+    "url": "/Starwars/static/media/logo.c91748d9.svg"
+  }
+]);
